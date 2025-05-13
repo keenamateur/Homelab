77ada16bd4458a3389cdbc3bc4a51a6715a77d60
keenamateur <75926358+keenamateur@users.noreply.github.com>
2025-05-13 22:25:57 +0200

    Update iptables-rules.sh

--- Wireguard-NordVpn/iptables-rules.sh
+++ Wireguard-NordVpn/iptables-rules.sh
@@ -17,22 +17,23 @@ VPN_SUBNET=$(docker network inspect -f '{{range .IPAM.Config}}{{.Subnet}}{{end}}
 #vpnbridge
 ip route del $WG_SUBNET via $VPN_IP dev proxy_net
 ip route add $WG_SUBNET via $VPN_IP dev proxy_net
-iptables -D TCP -p tcp -m multiport --dports 20,21,81,139,445,900,3389,3480,8080,8123,58123 -s $VPN_IP -j ACCEPT
-iptables -I TCP 1 -p tcp -m multiport --dports 20,21,81,139,445,990,3389,3480,8080,8123,58123 -s $VPN_IP -j ACCEPT
-iptables -D TCP -p tcp -m multiport --dports 9000,9443,50053,53001,51831,51841,51851 -s $VPN_IP -j ACCEPT
-iptables -I TCP 1 -p tcp -m multiport --dports 9000,9443,50053,53001,51831,51841,51851 -s $VPN_IP -j ACCEPT
-
+iptables -D TCP -p tcp -m multiport --dports 20,21,81,139,445 -s $VPN_IP -j ACCEPT
+iptables -I TCP 1 -p tcp -m multiport --dports 20,21,81,139,445 -s $VPN_IP -j ACCEPT
+iptables -D TCP -p tcp -m multiport --dports 9000,9443,51821 -s $VPN_IP -j ACCEPT
+iptables -I TCP 1 -p tcp -m multiport --dports 9000,944351821 -s $VPN_IP -j ACCEPT
 
 # WG->NORD
 docker exec -it $VPN_CONTAINER iptables -A FORWARD -i wg0 -o tun0 -j ACCEPT
 docker exec -it $VPN_CONTAINER iptables -A FORWARD -i tun0 -o wg0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 docker exec -it $VPN_CONTAINER iptables -t nat -A POSTROUTING -s $WG_SUBNET -o tun0 -j MASQUERADE
+
 # LAN
 docker exec -it $VPN_CONTAINER iptables -A FORWARD -i wg0 -s $WG_SUBNET -o eth0 -d $LAN_SUBNET -j ACCEPT
 docker exec -it $VPN_CONTAINER iptables -A FORWARD -i eth0 -o wg0 -s $LAN_SUBNET -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 docker exec -it $VPN_CONTAINER iptables -t nat -A POSTROUTING -s $WG_SUBNET -o eth0 -d $LAN_SUBNET -j MASQUERADE
 docker exec -it $VPN_CONTAINER iptables -t nat -A POSTROUTING -s $WG_SUBNET -o eth0 -d $VPN_SUBNET -j MASQUERADE
 docker exec -it $WG_CONTAINER ip route add $LAN_SUBNET via 172.30.10.1 dev eth0
+
 # IP ROUTE
 docker exec -it nodered ip route del $WG_SUBNET via $VPN_IP dev eth0
 docker exec -it nodered ip route add $WG_SUBNET via $VPN_IP dev eth0
